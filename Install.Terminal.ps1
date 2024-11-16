5e1d9357febe339eee25d384633fed908b263b89
Jean Carneiro do Nascimento <jeancnasc@hotmail.com>
2024-11-16 09:10:00 -0300

    feat[terminal]: instalando Windows Terminal, Clink e Oh My Posh

---
+++ Install.Terminal.ps1
@@ -0,0 +1,71 @@
+$ErrorActionPreference = 'Stop'
+
+$postInstallHome = $env:POST_INSTALL_HOME ?? $env:OneDrive
+$configPath = Join-Path -Path $postInstallHome -ChildPath Config
+Write-Output "Pasta de Configuração: $configPath"
+if(!(Test-Path -PathType Container -Path $configPath)){
+    New-Item -Type Directory $configPath | Out-Null
+}
+
+# --------------------------------------------------------------------------------------------------------------
+
+Write-Output 'Instalando Windows Terminal'
+winget install 'Microsoft.WindowsTerminal'
+
+$configPathWinTerm = Join-Path -Path $configPath -ChildPath "WindowsTerminal" 
+$settingsPathOriginal = Join-Path -Path $env:LOCALAPPDATA -ChildPath "\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+$settingsPathConfig = Join-Path -Path $configPathWinTerm -ChildPath "settings.json" 
+
+If(!(Test-Path -PathType Leaf -Path $settingsPathConfig)){
+    New-Item -ItemType Directory -Path $configPathWinTerm | Out-Null
+    Copy-Item -Path $settingsPathOriginal -Destination $settingsPathConfig -Force | Out-Null
+}
+New-Item -ItemType SymbolicLink -Path $settingsPathOriginal -Target $settingsPathConfig -Force | Out-Null
+
+# --------------------------------------------------------------------------------------------------------------
+
+Write-Output 'Instalando o Clink'
+winget install 'chrisant996.Clink'
+
+$configPathClink = Join-Path -Path $configPath -ChildPath "Clink" 
+
+If(!(Test-Path -PathType Container -Path $configPathClink)){
+    New-Item -ItemType Directory -Path "$configPathClink" | Out-Null
+}
+[Environment]::SetEnvironmentVariable('CLINK_SETTINGS', $configPathClink, 'User')
+[Environment]::SetEnvironmentVariable('CLINK_PATH', $configPathClink, 'User')
+
+# --------------------------------------------------------------------------------------------------------------
+
+Write-Output 'Instalando Oh My Posh'
+winget install 'JanDeDobbeleer.OhMyPosh'
+
+
+
+$configPathPOSH = Join-Path -Path $configPath -ChildPath "OhMyPosh" 
+
+$env:POSH_THEMES_PATH = [System.Environment]::GetEnvironmentVariable("POSH_THEMES_PATH","User") 
+$defaultPoshTheme = Join-Path -Path $env:POSH_THEMES_PATH -ChildPath "jandedobbeleer.omp.json"
+$poshTheme = Join-Path -Path $configPathPOSH -ChildPath "theme.omp.json"
+
+If(!(Test-Path -PathType Container -Path $configPathPOSH)){
+    New-Item -ItemType Directory -Path "$configPathPOSH" | Out-Null
+    Copy-Item -Path $defaultPoshTheme -Destination $poshTheme -Force | Out-Null
+}
+
+# Integrar com PowerShell
+
+if(!(Test-Path -PathType Leaf -Path $PROFILE)){
+    New-Item -Path $PROFILE -Force | Out-Null
+}
+@("if (Get-Command oh-my-posh -ErrorAction SilentlyContinue){ oh-my-posh init pwsh --config `"$poshTheme`" | Invoke-Expression }") + $(Get-Content -Force $PROFILE | Select-String oh-my-posh -NotMatch) | Set-Content $PROFILE
+
+# Integrar com Clink
+
+$poshClinkScript = Join-Path -Path $configPathClink -ChildPath "oh-my-posh.lua"
+"if( os.execute('where /Q oh-my-posh') ) then load(io.popen('oh-my-posh init cmd --config `"$($poshTheme -replace '\\', '/')`" '):read(`"*a`"))() end" | Set-Content $poshClinkScript
+
+# Instalar Fontes
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+oh-my-posh font install cascadiamono
--- Install.bat
+++ Install.bat
@@ -1,3 +1,5 @@
 @echo off
 echo 'Instalando o PowerShell'
 winget install "Microsoft.PowerShell"
+
+pwsh -File %~dp0\Install.Terminal.ps1
